6a316a2a92708cf2a45c6f513a82b7b6cb38efd7
Kyle Sweigart <kasweigart@gmail.com>
2020-07-21 18:10:01 -0400

    added homepage image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { Jumbotron, Button } from "reactstrap";
+import space from "../images/space.jpg";
 
 const Home = () => {
   return (
     <div className="container mt-5">
       <Jumbotron style={{ backgroundColor: "rgb(248,249,250,0.75)" }}>
-        
-          <h1 className="display-3 justify-content-center d-flex">
-            Welcome to SpaceFollow
-          </h1>
-          <h2 className="text-center">
-            An organized platform for space exploration topics and data.
-          </h2>
-          <hr className="text-center" />
-          <h3 className='text-center'>
-            Explore the tabs above to learn more!
-          </h3>
-        
+        <h1 className="display-3 justify-content-center d-flex">
+          Welcome to SpaceFollow
+        </h1>
+        <h2 className="text-center">
+          An organized platform for space exploration topics and data.
+        </h2>
+        <hr className="text-center" />
+        <h3 className="text-center">Explore the tabs above to learn more!</h3>
+        <img src={space} alt="" className='img-fluid mt-2'/>
       </Jumbotron>
     </div>
   );
